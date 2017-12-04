a0b917c40501885fe24ae5b97159ffb2dc6dbf26
KITT\nixin <nixin3228036@gmail.com>
2017-12-04 16:15:38 +0800

    update

--- js/component-counter.js
+++ js/component-counter.js
@@ -1,5 +1,5 @@
 Vue.component('my-counter', {
-    template: '<button class="my-component-counter" v-on:click="counter += 1">{{ counter }}</button>',
+    template: '<button class="my-component-counter" @click="counter += 1">{{ counter }}</button>',
     data: function () {
         return {
             counter: 0
