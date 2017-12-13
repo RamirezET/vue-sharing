4e0e21f565295a702b4e7b76540dadb941271ce8
KITT\nixin <nixin3228036@gmail.com>
2017-12-13 16:59:12 +0800

    add  simple demo:comment component

--- js/components/componentDemoComment.js
+++ js/components/componentDemoComment.js
@@ -3,44 +3,9 @@
  */
 
 Vue.component('component-demo-comment', {
-    template: '<div id="shoppingCart" v-cloak>' +
-    '    <div class="item" v-if="itempricelist.length">' +
-    '        <table class="shopping-cart-table">' +
-    '            <thead>' +
-    '            <tr>' +
-    '                <th>序号</th>' +
-    '                <th>图片</th>' +
-    '                <th>名称</th>' +
-    '                <th>单价</th>' +
-    '                <th>数量</th>' +
-    '                <th>小计</th>' +
-    '            </tr>' +
-    '            </thead>' +
-    '            <tbody>' +
-    '            <tr v-for="(itemList,index) in itempricelist ">' +
-    '                <td>' +
-    '                    <button @click="removeItem(index)">{{index+1}}</button>' +
-    '                </td>' +
-    '                <td class="item-pic">' +
-    '                    <img :src=\'"https://img11.360buyimg.com/n2/"+itemList.productLogo\' alt="">' +
-    '                </td>' +
-    '                <td class="item-name">{{itemList.productName}}</td>' +
-    '                <td>¥{{itemList.currentPrice}}</td>' +
-    '                <td class="item-count">' +
-    '                    <button @click="reduceCount(index)"' +
-    '                            :disabled="itemList.isBuy==1">-' +
-    '                    </button>' +
-    '                    {{itemList.isBuy}}' +
-    '                    <button @click="addCount(index)">+</button>' +
-    '                </td>' +
-    '                <td>¥{{itemList.isBuy*itemList.currentPrice}}</td>' +
-    '            </tr>' +
-    '            </tbody>' +
-    '        </table>' +
-    '        <div class="total-price">总价格：¥{{totalPrice}}</div>' +
-    '    </div>' +
-    '    <div class="empty-box" v-else>穷*！购物车里都没东西，跟咸鱼有什么分别！</div>' +
-    '</div>',
+    template:
+        '<div v-cloak>' +
+        '</div>',
     props: ['itempricelist'],
     methods: {
         addCount: function (index) {
