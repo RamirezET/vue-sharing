5ed4084237a7e97eb0f937321516dbcaa09002c9
KITT\nixin <nixin3228036@gmail.com>
2017-12-14 15:31:36 +0800

    评论列表组件化

--- js/components/componentDemoComment.js
+++ js/components/componentDemoComment.js
@@ -4,29 +4,117 @@
 
 Vue.component('component-demo-comment', {
     template:
-        '<div v-cloak>' +
-        '</div>',
-    props: ['itempricelist'],
+    '    <div class="component-comment-area">\n' +
+    '        <div class="split-line">\n' +
+    '            <i></i>\n' +
+    '            <p>评论区</p>\n' +
+    '        </div>\n' +
+    '        <div class="comment-list" v-if="commentlistdata" v-for="(commentListItem,index) in commentlistdata"\n' +
+    '             :key="commentListItem.movieCommentId" v-cloak>\n' +
+    '            <div class=" comment-list-left">\n' +
+    '                <img :src="commentListItem.userHeadImg" alt="">\n' +
+    '            </div>\n' +
+    '            <div class="comment-list-right">\n' +
+    '                <p class="comment-list-right-name">{{commentListItem.userNickName}}</p>\n' +
+    '                <p class="comment-list-right-comment">{{commentListItem.comment}}</p>\n' +
+    '                <div class="comment-list-right-img-box" v-if="commentListItem.commentImg">\n' +
+    '                    <div class="comment-list-right-img-item"\n' +
+    '                         v-for="commentImage in commentListItem.commentImg.split(\',\')">\n' +
+    '                        <img :src="commentImage" alt="">\n' +
+    '                    </div>\n' +
+    '                    <div class="clearfix"></div>\n' +
+    '                </div>\n' +
+    '                <div class="comment-list-right-video-box" v-if="commentListItem.commentVideo">\n' +
+    '                    <video controls="" x5-video-player-type="h5" x5-video-player-fullscreen="true"\n' +
+    '                           playsinline="true" height="182">\n' +
+    '                        <source :src="commentListItem.commentVideo" type="video/mp4">\n' +
+    '                    </video>\n' +
+    '                </div>\n' +
+    '                <div class="comment-list-right-bottom">\n' +
+    '                    <div class="comment-list-right-bottom-left">\n' +
+    '                        <div class="comment-list-right-bottom-left-datetime">{{commentListItem.createDate}}</div>\n' +
+    '                        <div class="comment-list-right-bottom-right-deletebtn"\n' +
+    '                             @click="deleteComment(commentListItem.movieCommentId,index)">删除\n' +
+    '                        </div>\n' +
+    '                    </div>\n' +
+    '                    <div class="comment-list-right-bottom-right">\n' +
+    '                        <div class="comment-list-right-bottom-right-like">\n' +
+    '                            <img :src="commentListItem.isLike|isLike" alt=""\n' +
+    '                                 @click="addLike(commentListItem.movieCommentId,index)">\n' +
+    '                            <span>{{commentListItem.likeNum}}</span>\n' +
+    '                        </div>\n' +
+    '                        <div class="comment-list-right-bottom-right-comment">\n' +
+    '                            <img src="images/comment.png" alt=""\n' +
+    '                                 @click="goToCommentPage(commentListItem.movieCommentId)">\n' +
+    '                            <span>{{commentListItem.commentNum}}</span>\n' +
+    '                        </div>\n' +
+    '                    </div>\n' +
+    '                </div>\n' +
+    '            </div>\n' +
+    '        </div>\n' +
+    ' </div>',
+    props: ['commentlistdata', 'accseetoken'],
     methods: {
-        addCount: function (index) {
-            this.itempricelist[index].isBuy++
+        goToCommentPage: function (movieCommentId) {
+            console.log(movieCommentId);
         },
-        reduceCount: function (index) {
-            if (this.itempricelist[index].isBuy !== 1) {
-                this.itempricelist[index].isBuy--
-            }
+        addLike: function (movieCommentId, index) {
+            var self = this;
+            axios({
+                url: 'http://10.30.3.124:8080/rest.web/rest/movie-bigMovie-resource/addMovieCommentThumbsUp',
+                method: 'POST',
+                data: {
+                    movieCommentId: movieCommentId,
+                    accessToken: self.accseetoken,
+                    isUp: !self.commentlistdata[index].isLike
+                },
+                transformRequest: [function (data) {
+                    let ret = ''
+                    for (let it in data) {
+                        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+                    }
+                    return ret
+                }],
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(function (response) {
+                if (self.commentlistdata[index].isLike) {
+                    self.commentlistdata[index].likeNum--;
+                } else {
+                    self.commentlistdata[index].likeNum++;
+                }
+                self.commentlistdata[index].isLike = !self.commentlistdata[index].isLike;
+            })
         },
-        removeItem: function (index) {
-            this.itempricelist.splice(index, 1)
+        deleteComment: function (commentId, index) {
+            var self = this;
+            axios({
+                url: 'http://10.30.3.124:8080/rest.web/rest/movie-comment-resource/deleteComment',
+                method: 'POST',
+                data: {
+                    commentId: commentId,
+                    accessToken: self.accseetoken,
+                    type: 'PLATFORM'
+                },
+                transformRequest: [function (data) {
+                    let ret = ''
+                    for (let it in data) {
+                        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+                    }
+                    return ret
+                }],
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(function (response) {
+                self.commentlistdata.splice(index, 1);
+            })
         }
     },
-    computed: {
-        totalPrice: function () {
-            var totalPrice = 0;
-            for (var i = 0; i < this.itempricelist.length; i++) {
-                totalPrice += this.itempricelist[i].currentPrice * this.itempricelist[i].isBuy
-            }
-            return totalPrice
+    filters: {
+        isLike: function (isLike) {
+            return isLike ? 'images/like2.png' : 'images/like1.png';
         }
     }
 });
--- js/simpleComponentDemo.js
+++ js/simpleComponentDemo.js
@@ -7,66 +7,6 @@ var vm = new Vue({
             commentListData: [],
             accessToken: ''
         },
-        methods: {
-            goToCommentPage: function (movieCommentId) {
-                console.log(movieCommentId);
-            },
-            addLike: function (movieCommentId, index) {
-                var self = this;
-                console.log(movieCommentId);
-                console.log(index);
-                axios({
-                    url: 'http://10.30.3.124:8080/rest.web/rest/movie-bigMovie-resource/addMovieCommentThumbsUp',
-                    method: 'POST',
-                    data: {
-                        movieCommentId: movieCommentId,
-                        accessToken: self.accessToken,
-                        isUp: !self.commentListData[index].isLike
-                    },
-                    transformRequest: [function (data) {
-                        let ret = ''
-                        for (let it in data) {
-                            ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
-                        }
-                        return ret
-                    }],
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }
-                }).then(function (response) {
-                    if (self.commentListData[index].isLike) {
-                        self.commentListData[index].likeNum--;
-                    } else {
-                        self.commentListData[index].likeNum++;
-                    }
-                    self.commentListData[index].isLike = !self.commentListData[index].isLike;
-                })
-            },
-            deleteComment: function (commentId, index) {
-                var self = this;
-                axios({
-                    url: 'http://10.30.3.124:8080/rest.web/rest/movie-comment-resource/deleteComment',
-                    method: 'POST',
-                    data: {
-                        commentId: commentId,
-                        accessToken: self.accessToken,
-                        type: 'PLATFORM'
-                    },
-                    transformRequest: [function (data) {
-                        let ret = ''
-                        for (let it in data) {
-                            ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
-                        }
-                        return ret
-                    }],
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }
-                }).then(function (response) {
-                    self.commentListData.splice(index, 1);
-                })
-            }
-        },
         mounted: function () {
             var self = this;
             axios({
@@ -118,10 +58,6 @@ var vm = new Vue({
                 })
             })
         },
-        filters: {
-            isLike: function (isLike) {
-                return isLike ? 'images/like2.png' : 'images/like1.png';
-            }
-        }
+
     })
 ;
