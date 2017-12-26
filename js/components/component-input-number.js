f85a0e3609405db5f4285d04d707d4ca694740d2
Ramirez <nixin3228036@gmail.com>
2017-12-26 23:00:53 +0800

    learning component

--- js/components/component-input-number.js
+++ js/components/component-input-number.js
@@ -2,7 +2,7 @@
  * Created by Ramirez on 12/23/2017.
  */
 Vue.component('input-number', {
-    template: '<div class="input-number"><input type="number" :value="currentValue"></div>',
+    template: '<div class="input-number"><input type="number" :value="currentValue" @change="handleChange"><button @click="handleDown" :disabled="currentValue<=min">-</button><button @click="handleUp" :disabled="currentValue>=max">+</button></div>',
     props: {
         max: {
             type: Number,
@@ -24,13 +24,12 @@ Vue.component('input-number', {
     },
     watch: {
         currentValue: function (val) {
-            console.log(val);
             this.$emit('input', val);
             this.$emit('on-change', val)
         },
         value: function (val) {
-            console.log(val);
-            this.updateValue(val);
+
+            this.updateValue(Number(val));
         }
     },
     methods: {
@@ -41,6 +40,31 @@ Vue.component('input-number', {
                 val = this.min
             }
             this.currentValue = val;
+        },
+        handleChange: function (event) {
+            var val = event.target.value.trim();
+            var min = this.min;
+            var max = this.max;
+            this.currentValue = val;
+            if (val > max) {
+                this.currentValue = max;
+            } else if (val < min) {
+                this.currentValue = min();
+            }
+        },
+        handleDown: function () {
+            if (this.currentValue <= this.min) {
+                return
+            } else {
+                this.currentValue--;
+            }
+        },
+        handleUp: function () {
+            if (this.currentValue >= this.max) {
+                return
+            } else {
+                this.currentValue++;
+            }
         }
     },
     mounted: function () {
